1b9a77f5aa067c2318ebec1223034dc4a87ec982
Isabella Ruiz Velasquez <isruve0358@gmail.com>
2026-03-27 20:34:18 -0500

    Realizacion de la clase principal Mascotas con sus atributos definidos
    Definicion del constructor para ID, nombre, especie, raza, peso y edad. Se incluye el metodo preparar_datos_tabla para facilitar la representacion visual de la informacion.

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from datetime import datetime
 from tabulate import tabulate
+
+class Mascota:
+    def __init__(self, id_mascota, nombre, especie, raza, peso, edad):
+        self.id_mascota = id_mascota
+        self.nombre = nombre
+        self.especie = especie  
+        self.raza = raza
+        self.peso = peso       
+        self.edad = edad
