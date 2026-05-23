ae2d52abb2d2736a52c1306fd0450d1b46c0739f
Isabella Ruiz Velasquez <isruve0358@gmail.com>
2026-05-22 23:33:14 -0500

    Implementar clase abstracta Mascota

---
+++ modelos.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Mascota(ABC):
+    def __init__(self, id_mascota: str, nombre: str, especie: str, raza: str, sexo: str, peso: float, edad: int, nombre_dueno: str, fecha_registro: str):
+        self.id_mascota: str = id_mascota
+        self.nombre: str = nombre
+        self.especie: str = especie  
+        self.raza: str = raza
+        self.sexo: str = sexo
+        self.peso: float = peso       
+        self.edad: int = edad
+        self.nombre_dueno: str = nombre_dueno
+        self.fecha_registro: str = fecha_registro 
+        
+    def obtener_especie(self) -> str:
+        return self.especie
+
+    def obtener_peso(self) -> float:
+        return self.peso
+
+    @abstractmethod
+    def revisar_paciente(self) -> str:
+        pass
