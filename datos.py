eb50d5cee12aa381ff20da18f04a4ed6ae2285e1
Isabella Ruiz Velasquez <isruve0358@gmail.com>
2026-05-22 23:26:07 -0500

    Implementar DICCIONARIO_RAZAS para aves, gatos y perros

---
+++ datos.py
@@ -0,0 +1,3 @@
+DICCIONARIO_RAZAS: dict[str, list[str]] = {"Ave": ["Agapornis", "Cacatúa", "Canario", "Cotorra", "Guacamaya", "Jilguero", "Loro", "Ninfa", "Periquito", "Ruiseñor"],
+                                           "Gato": ["Angora", "Azul Ruso", "Bengala", "Criollo", "Esfinge", "Maine Coon", "Persa", "Ragdoll", "Siamés", "Siberiano"],
+                                           "Perro": ["Beagle", "Boxer", "Bulldog", "Chihuahua", "Golden Retriever", "Labrador", "Pastor Alemán", "Poodle", "Pug", "Shih Tzu", "Criollo"]}
