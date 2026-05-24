c927b695f3e68333905ecea51ef0fcf487b0ec98
Isabella Ruiz Velasquez <isruve0358@gmail.com>
2026-05-23 22:23:24 -0500

    Importar modelos, validaciones y excepciones para gestión de mascotas

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import os
+import tkinter as tk
+from tkinter import messagebox, ttk
+from datetime import datetime
 
+from datos import DICCIONARIO_RAZAS
+from modelos import Perro, Gato, Ave, Medicamento, ValidarDatos
+from excepciones import PesoInvalidoError, MedicamentoPeligrosoError
